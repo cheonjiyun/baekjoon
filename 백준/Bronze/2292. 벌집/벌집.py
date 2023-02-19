ebebdd23266368eb0a06c3bdbd743251e334e305
CheonJiyun <70828192+gunbambam@users.noreply.github.com>
2023-02-19 21:38:24 +0900

    [Bronze II] Title: 벌집, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -4,15 +4,8 @@ number = int(sys.stdin.readline())
 
 sum = 1
 count = 1
-for i in range(1, 1000000000):
-    if number == 1:
-        break
-
+while sum < number:
+    sum = sum + (count * 6)
     count += 1
-    sum = sum + (i * 6)
-
-    if sum >= number:
-        break
-
 
 print(count)
