453ae244cf9b0b23d045b86cf8315e40e83d77bd
천지윤 CheonJiyun <70828192+cheonjiyun@users.noreply.github.com>
2023-07-11 17:29:08 +0900

    [level 0] Title: 중앙값 구하기, Time: 10.39 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(array: IntArray): Int {
+        
+        array.sort()        
+        
+        var answer: Int = array[array.size / 2]
+        return answer
+    }
+}
