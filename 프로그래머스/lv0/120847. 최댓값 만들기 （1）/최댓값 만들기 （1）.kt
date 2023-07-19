47f1cb2c76a8860bb49b5fac75f7ba52efef3810
천지윤 CheonJiyun <70828192+cheonjiyun@users.noreply.github.com>
2023-07-19 18:13:55 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 12.95 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(numbers: IntArray): Int {
+        numbers.sort()    
+        
+        var answer: Int = numbers[numbers.size-1] * numbers[numbers.size-2]
+        return answer
+    }
+}
