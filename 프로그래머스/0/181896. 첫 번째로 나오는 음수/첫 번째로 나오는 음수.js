924c6761e25a88802e032660bb6e5c2bcfd7070b
천지윤 CheonJiyun <70828192+cheonjiyun@users.noreply.github.com>
2024-02-17 22:39:30 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,10 +1,3 @@
-function solution(num_list) {
-    let answer = -1;
-    for(let i = 0; i < num_list.length; i++){
-        if(num_list[i] < 0){
-            answer = i;
-            break;
-        };
-    }
-    return answer;
+function solution(num_list) {    
+    return num_list.findIndex((el) => el < 0);
 }
