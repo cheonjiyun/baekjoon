f4cef32c19f80112e01acdf2a4da7e917cdba57d
천지윤 CheonJiyun <70828192+cheonjiyun@users.noreply.github.com>
2024-02-17 23:04:13 +0900

    [level 0] Title: 2의 영역, Time: 0.30 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,15 +1,6 @@
 function solution(arr) {
-    const index = [];
-    arr.forEach((el,i) => el === 2 ? index.push(i) : null)
+    const start = arr.indexOf(2);
+    const end = arr.lastIndexOf(2);
     
-    let answer = [];
-    if (index.length === 0){
-        answer.push(-1);
-    }else if(index.length === 1){
-        answer.push(arr[index[0]]);
-    }else{
-        answer = arr.slice(index[0], index[index.length-1]+1)
-    }
-    
-    return answer
+    return end === -1 ? [-1] : arr.slice(start, end+1);
 }
