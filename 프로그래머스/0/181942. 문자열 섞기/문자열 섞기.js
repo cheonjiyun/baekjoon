94e42a83467f32fc92e62f8aa1434dc70273a01b
천지윤 CheonJiyun <70828192+cheonjiyun@users.noreply.github.com>
2024-02-20 16:57:30 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,7 +1,8 @@
 function solution(str1, str2) {
-    let answer = ""
-    for(let i = 0; i < str1.length; i++){
-        answer += str1[i]+ str2[i]
-    }
-    return answer;
+    // let answer = ""
+    // for(let i = 0; i < str1.length; i++){
+    //     answer += str1[i]+ str2[i]
+    // }
+    
+    return [...str1].map((el, i) => el += str2[i]).join('');
 }
