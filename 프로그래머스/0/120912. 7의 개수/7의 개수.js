fe2b8b4f11aa24d5cc39bf329b0dd65d9e9f592a
천지윤 CheonJiyun <70828192+cheonjiyun@users.noreply.github.com>
2024-02-22 15:51:13 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -1,7 +1,8 @@
 function solution(array) {
-    let answer = 0;
-    for(let i  = 0; i < array.length; i++){
-        answer += Array.from('' + array[i]).filter(e => e == 7).length
-    }
-    return answer;
+    // let answer = 0;
+    // for(let i  = 0; i < array.length; i++){
+    //     answer += Array.from('' + array[i]).filter(e => e == 7).length
+    // }
+    // return asnwer;
+    return array.join('').split('7').length - 1
 }
