69aad9aef5f29b52bbc930a1fa0e6e2c62e464a0
천지윤 CheonJiyun <70828192+cheonjiyun@users.noreply.github.com>
2024-02-27 17:30:20 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,6 +1,4 @@
 function solution(emergency) {
-    let result = []
-    const emSort = emergency.slice().sort((a,b) => b - a)
-    
+    const emSort = [...emergency].sort((a,b) => b - a)    
     return emergency.map(el => emSort.indexOf(el)+1)
 }                       
