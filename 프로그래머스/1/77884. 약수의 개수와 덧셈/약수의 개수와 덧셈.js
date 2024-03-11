dd32a2d4bc7373d8d94785c0e33e180f8aaf11da
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-03-11 14:28:27 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -3,11 +3,7 @@ function solution(left, right) {
     let answer = 0
     //약수 구하기
     for(let number = left; number <= right; number++){        
-        let primeCount = 0
-        for(let i = 1; i <= number; i++){
-            if(number % i == 0) primeCount++
-        }
-        primeCount % 2 ? answer -= number : answer += number
+        Number.isInteger(Math.sqrt(number)) ? answer -= number : answer+= number
     }
     return answer;
 }
