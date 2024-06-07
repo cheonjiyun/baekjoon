b245f4931fb9f5582007b1f92198b10b8961842a
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-06-07 19:12:43 +0900

    [level 3] Title: 네트워크, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -7,18 +7,20 @@ function solution(n, computers) {
     for(let i = 0; i < n; i++){
         if(computerArr[i] == 1) continue; // 이미 방문한 컴퓨터
         
-        q.push(i)
+        result++ // 네트워크 개수 추가
+        
+        q.push(i) // 방문할 컴퓨터 추가
         while(q.length){
-            const node = q.shift();
+            const node = q.shift(); // 방문할 컴퓨터
             computerArr[node] = 1; // 방문확인
             
-            for(let c = 0; c < n; c++){
-                if(computers[node][c] == 1 && computerArr[c] != 1){ // 연결된컴퓨터 && 방문안한컴퓨터
-                    q.push(c)
+            for(let c = 0; c < n; c++){ // 연결된 컴퓨터가 있는지 돌면서 확인
+                if(computers[node][c] == 1 && computerArr[c] != 1){ // 연결된컴퓨터가 있음 && 방문안한컴퓨터다
+                    q.push(c) // 방문할 예정
                 }
             }
         }
-        result++
+        
     }
         
     return result;
