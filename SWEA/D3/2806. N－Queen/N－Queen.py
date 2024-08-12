781ccbde654cca37908d18c73cd617359c1ecba4
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-08-12 14:30:16 +0900

    [D3] Title: N-Queen, Time: 251 ms, Memory: 53,344 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,6 +1,3 @@
-
-cnt = 0
-
 def isPossible(chess, row, x):
     for y in range(row):
         if chess[y] == x or abs(x - chess[y]) == abs(row - y):
@@ -8,18 +5,18 @@ def isPossible(chess, row, x):
     return True
 
 def nQueen(chess, n, row):
-    global cnt
-
     if row == n:
-        cnt += 1
-        return
+        return 1
 
+    result = 0
     # 가로 한줄
     for x in range(n):
         # 놓을 수 있느냐
         if isPossible(chess, row, x):
             chess[row] = x
-            nQueen(chess, n, row+1)
+            result += nQueen(chess, n, row+1)
+
+    return result
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
@@ -29,8 +26,7 @@ for test_case in range(1, T + 1):
     chess = [-1 for _ in range(n)]
 
     cnt = 0
-    nQueen(chess, n, 0)
+    result = nQueen(chess, n, 0)
 
-    print(f"#{test_case} {cnt}")
-
+    print(f"#{test_case} {result}")
 
