6f4bdfa939f83617c9cf264bb5c0cf4dc62d1707
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-09-12 11:21:51 +0900

    [D3] Title: 부분 수열의 합, Time: 885 ms, Memory: 62,256 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,26 +1,22 @@
+def dfs(arr, i, n, k, number):
+    if number == k:
+        return 1
+    if number > k:
+        return 0
+    if i == n:
+        return 0
+
+    result = 0
+    result += dfs(arr, i+1, n, k, number)
+    result += dfs(arr, i+1, n, k, number + arr[i])
+    return result
+
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     n, k = map(int, input().split())
     arr = list(map(int, input().split()))
 
-    answer =0
-    for i in range(0, 1 << n): # 2^n
-        # print("i::", bin(i))
-        sumNumber = 0
-        for j in range(n): # n
-            # print('j', j)
-            #
-            # print('왼', bin(i))
-            # print('오', bin(1 << j))
-            # print('&', bin(i & (1 << j)))
-            # print(int(i & (1 << j)))
-            if i & (1 << j):
-                # print('if문 통과')
-                sumNumber += arr[j]
-
-            # print()
-        if sumNumber == k:
-            answer += 1
+    answer = dfs(arr, 0, n, k, 0)
 
     print(f"#{test_case} {answer}")
