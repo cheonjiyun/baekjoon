9d9e1df9751283e74b6edf3a23e0912082c61eed
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-09-20 11:34:39 +0900

    [level 2] Title: 삼각 달팽이, Time: 116.42 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -7,44 +7,14 @@ function solution(n) {
     
     let numberN = 1
     let x = 0
-    let y = 0
-    let d = 0 
+    let y = -1
+
+    for(let i = n; i > 0; i -= 3){
+        for(let j = 0; j < i; j++) answer[++y][x] = numberN++;        
+        for(let j = 0; j < i-1; j++) answer[y][++x] = numberN++;        
+        for(let j = 0; j < i-2; j++) answer[--y][--x] = numberN++;
+        
+    }
     
-    const dx = [0, 1, -1]
-    const dy = [1, 0, -1]
-    
-    const isTurn = [n]
-    for(let i = 1; i < n; i++){
-        isTurn.push(n - i + isTurn[i-1])
-    } 
-    console.log(isTurn)
-    
-    while(numberN <= (n + 1) * (n / 2)){
-        // 현재 숫자
-        answer[y][x] = numberN
-        
-        
-        // Turn 조건
-        if(isTurn.includes(numberN)){
-            d = (d + 1) % 3
-        }
-        
-        
-        
-        // 다음 숫자
-        numberN += 1        
-        
-        // // 다음에 둘곳
-        x += dx[d]
-        y += dy[d]
-    
-    } 
-    
-    const result = []
-    answer.forEach((el) => {
-        el.forEach(e => {
-            result.push(e)
-        })        
-    })
-    return result;
+    return answer.flat();
 }
