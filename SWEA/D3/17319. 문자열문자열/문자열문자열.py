42b3d5e8e223dcc0191c7e0b931fea541c3d3d07
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-09-23 21:31:07 +0900

    [D3] Title: 문자열문자열, Time: 126 ms, Memory: 43,484 KB -BaekjoonHub

---
+++ SWEA/D3/17319. 문자열문자열/문자열문자열.py
@@ -0,0 +1,7 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    n = int(input())
+    s = input()
+
+    print(f"#{test_case} {'Yes' if s[:n//2] == s[n//2:] else 'No'}")
