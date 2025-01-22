50b4a7fcd8b5f6156c11425e95577d4bf95bf6e5
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-01-22 20:50:23 +0900

    [Bronze V] Title: 꼬마 정민, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        long sum = 0;
-        String line = scanner.nextLine();
-        Scanner lineScanner = new Scanner(line);
-
-        while (lineScanner.hasNext()) {
-            sum += lineScanner.nextLong();
-        }
-
-        System.out.println(sum);
-
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long sum = 0;
+		
+		String line = sc.nextLine();
+		Scanner lineScanner = new Scanner(line);
+		
+		while(lineScanner.hasNext()) {
+			sum += lineScanner.nextLong();
+		}
+		
+		System.out.println(sum);		
+	}
 }
