f4423811772067d56452e69fac36ed5a7fefd6a4
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-02-10 20:24:38 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 129 ms, Memory: 30,592 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,26 +1,25 @@
-
 import java.io.FileInputStream;
 import java.util.HashSet;
 import java.util.Scanner;
-
+ 
 class Solution {
   public static void main(String args[]) throws Exception {
-
+ 
     Scanner sc = new Scanner(System.in);
     int T;
     T = sc.nextInt();
-
+ 
     for (int test_case = 1; test_case <= T; test_case++) {
       int number = sc.nextInt();
-
+ 
       HashSet<Integer> count = new HashSet<Integer>();
-
+ 
       int i = 1;
       int result = 0;
       while (count.size() < 10) {
         int ni = number * i;
         result = ni;
-
+ 
         for (String s : String.valueOf(ni).split("")) {
           count.add(Integer.valueOf(s));
         }
