951c926b34f578f686e06c263313d8a39f726f8c
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-02-17 14:46:58 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 114 ms, Memory: 47,116 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -1,20 +1,56 @@
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    n = int(input())
-    arr = list(map(int, input().split()))
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
-    dic = {}
-    for x in arr:
-        if x in dic:
-            dic[x] += 1
-        else:
-            dic[x] = 1
+public class Solution {
 
-    frequency = 0
-    num = 0
-    for x in dic.keys():
-        if dic[x] > frequency or (dic[x] == frequency and x > num):
-            frequency = dic[x]
-            num = x
-    print(f"#{test_case} {num}")
+  private static int[] counter;
+
+  public static void main(String[] args) throws Exception {
+
+    /**
+     * 0. 입력파일 읽어들이기
+     */
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    // 결과를 한 번에 출력하기 위한 StringBuilder
+    StringBuilder sb = new StringBuilder();
+
+    int T;
+    T = Integer.parseInt(in.readLine());
+    for (int test_case = 1; test_case <= T; test_case++) {
+      sb.append("#" + test_case + " ");
+
+      // 여러분의 알고리즘 코드 작성하기
+      /**
+       * 1. 입력 파일 객체화
+       */
+      in.readLine();
+      String[] scores = in.readLine().split(" ");
+      counter = new int[101];
+
+
+      /*
+       * 2. 알고리즘 풀기
+       */
+      for (int i = 0; i < scores.length; i++) {
+        int score = Integer.parseInt(scores[i]);
+        counter[score]++; // 점수카운팅
+      }
+      /*
+       * 정답 출력
+       */
+      int max = Integer.MIN_VALUE;
+      int answer = -1;
+      for (int score = 0; score <= 100; score++) {
+        // 같다까지 해야 가장 큰 점수가 answer에 저장됨
+        if (max <= counter[score]) {
+          max = counter[score];
+          answer = score;
+        }
+      }
+      sb.append(answer).append("\n");
+    }
+    System.out.println(sb);
+  }
+}
