c63f449a410ef6831b49963893bf310c7a948ebe
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-02-17 16:19:39 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 132 ms, Memory: 44,748 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,11 +1,80 @@
-for test_case in range(1, 11):
-    times = int(input())
-    boxes = list(map(int, input().split()))
-    boxes.sort() # 정렬
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
-    for _ in range(times):
-        boxes[len(boxes)-1] -= 1 # 1등 하나 내림
-        boxes[0] += 1 # 꼴찌 올림
-        boxes.sort()
+public class Solution {
+  public static void main(String[] args) throws Exception {
 
-    print(f"#{test_case} {boxes[len(boxes)-1] -boxes[0]}")
+    /**
+     * 0. 입력파일 읽어들이기
+     */
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    // 결과를 한 번에 출력하기 위한 StringBuilder
+    StringBuilder sb = new StringBuilder();
+
+    int T = 10;
+
+    for (int test_case = 1; test_case <= T; test_case++) {
+      sb.append("#" + test_case + " ");
+
+      // 여러분의 알고리즘 코드 작성하기
+
+      /*
+       * 1. 입력 파일 객체화
+       */
+      int dump = Integer.parseInt(in.readLine());
+      String[] inputs = in.readLine().split(" ");
+      int[] width = new int[100];
+      for (int i = 0; i < inputs.length; i++) {
+        width[i] = Integer.parseInt(inputs[i]);
+      }
+
+      /*
+       * 2. 알고리즘 풀기
+       */
+      for (int i = 0; i < dump; i++) {
+        int maxIndex = getMaxIndex(width);
+        int minIndex = getMinIndex(width);
+
+        width[minIndex] += 1;
+        width[maxIndex] -= 1;
+      }
+
+      /*
+       * 정답 출력
+       */
+      int maxIndex = getMaxIndex(width);
+      int minIndex = getMinIndex(width);
+      int result = width[maxIndex] - width[minIndex];
+      sb.append(result).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+  public static int getMinIndex(int[] width) {
+    int min = Integer.MAX_VALUE;
+    int idx = 0;
+    for (int i = 0; i < width.length; i++) {
+      if (width[i] <= min) {
+        min = width[i];
+        idx = i;
+      }
+    }
+    return idx;
+  }
+
+
+  public static int getMaxIndex(int[] width) {
+    int max = Integer.MIN_VALUE;
+    int idx = 0;
+    for (int i = 0; i < width.length; i++) {
+      if (width[i] >= max) {
+        max = width[i];
+        idx = i;
+      }
+    }
+    return idx;
+  }
+}
