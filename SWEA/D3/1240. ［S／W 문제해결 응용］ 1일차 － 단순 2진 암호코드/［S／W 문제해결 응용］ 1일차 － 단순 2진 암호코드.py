2be3bdfe99974c67d59639cb348df78e66c019af
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-03-10 23:35:53 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 107 ms, Memory: 44,988 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,57 +1,88 @@
-cipherMap = {
-    '0001101': 0,
-    '0011001': 1,
-    '0010011': 2,
-    '0111101': 3,
-    '0100011': 4,
-    '0110001': 5,
-    '0101111': 6,
-    '0111011': 7,
-    '0110111': 8,
-    '0001011': 9,
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    StringTokenizer st = new StringTokenizer(br.readLine().trim());
+    int T = Integer.parseInt(st.nextToken());
+
+    for (int test_case = 1; test_case <= T; test_case++) {
+      sb.append("#" + test_case + " ");
+      st = new StringTokenizer(br.readLine().trim());
+
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+
+      int[][] arr = new int[n][m];
+
+      int x = 0;
+      int y = 0;
+      for (int i = 0; i < n; i++) {
+        st = new StringTokenizer(br.readLine().trim());
+        String[] input = st.nextToken().split("");
+        for (int j = 0; j < m; j++) {
+          int value = Integer.parseInt(input[j]);
+          arr[i][j] = value;
+
+          // 기록
+          if (value == 1) {
+            y = i;
+            x = j;
+          }
+          // System.out.print(arr[i][j]);
+        }
+        // System.out.println();
+      }
+
+      x -= 7 * 8; // 암호 시작점
+      x += 1;
+
+      HashMap<String, Integer> ciyper = new HashMap<String, Integer>();
+      ciyper.put("0001101", 0);
+      ciyper.put("0011001", 1);
+      ciyper.put("0010011", 2);
+      ciyper.put("0111101", 3);
+      ciyper.put("0100011", 4);
+      ciyper.put("0110001", 5);
+      ciyper.put("0101111", 6);
+      ciyper.put("0111011", 7);
+      ciyper.put("0110111", 8);
+      ciyper.put("0001011", 9);
+
+      List<String> numbers = new ArrayList<>();
+
+      for (int i = x; i < x + 7 * 8; i += 7) {
+        String num = "";
+
+        for (int j = i; j < i + 7; j++) {
+          num += arr[y][j];
+        }
+
+        numbers.add(num);
+      }
+
+      int correct = 0;
+      int sum = 0;
+      for (int i = 0; i < 8; i += 2) {
+        correct += ciyper.get(numbers.get(i));
+        sum += ciyper.get(numbers.get(i));
+      }
+      correct *= 3;
+      for (int i = 1; i < 8; i += 2) {
+        correct += ciyper.get(numbers.get(i));
+        sum += ciyper.get(numbers.get(i));
+      }
+
+      sb.append(correct % 10 == 0 ? sum : 0).append("\n");
+    }
+
+    System.out.println(sb);
+  }
 }
-
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    # 입력받기
-    N, M = map(int, input().split())
-    arr = [input() for _ in range(N)]
-
-    cipherNumbers = []
-    for line in arr:
-        # 암호문이 있는 줄 찾아내기
-        if line.rfind('1') != -1:
-            # 암호코드 인덱스 찾아내기
-            endIndex = line.rfind('1')
-            startIndex = endIndex - 55
-
-            # 암호숫자 8개 추출
-            for i in range(8):
-                code = line[startIndex + (i * 7) : startIndex + (i * 7) + 7]
-                cipherNumber = cipherMap[code]
-                cipherNumbers.append(cipherNumber)
-
-            break
-
-    # 홀수 숫자함 짝수 숫자합
-    oddSum = 0
-    evenSum = 0
-    for i in range(len(cipherNumbers)):
-        if i % 2 == 0:
-            oddSum += cipherNumbers[i]
-        else:
-            evenSum += cipherNumbers[i]
-
-
-    # 검증코드
-    verificationCode = oddSum * 3 + evenSum
-    verification = verificationCode % 10 == 0
-
-    if not verification:
-        result = 0
-    else:
-        result = oddSum + evenSum
-
-    # 출력
-    print(f"#{test_case} {result}")
