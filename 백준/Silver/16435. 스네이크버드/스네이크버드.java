3694683cfd5b485ac278901b0dd12747cf9f6ba0
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-03-11 14:14:22 +0900

    [Silver V] Title: 스네이크버드, Time: 108 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.java
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -1,7 +1,6 @@
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,34 +15,20 @@ public class Main {
 		int l = Integer.parseInt(st.nextToken());
 
 		int[] fruits = new int[n];
-		boolean[] eat = new boolean[n];
-
 		st = new StringTokenizer(br.readLine().trim());
 		for (int i = 0; i < n; i++) {
 			int value = Integer.parseInt(st.nextToken());
-			fruits[i] = value;
-			
-			if(value <= l) {
-				eat[i] = true;
-				l += 1;
-			}
+			fruits[i] = value;			
 		}
 		
-		boolean has;
-		while(true) {
-			has = false;
-			for(int i = 0; i < n; i++) { // 한바퀴 돌면서 작은 아이가 있는지
-				if(fruits[i] <= l && eat[i] == false) {
-					eat[i] = true;
-					l += 1;
-					has= true;
-				}
-			}
-			if(has == false) {// 한바퀴 돌았는데도 못먹었으면
-				break;
+		Arrays.sort(fruits);
+		
+		for(int i = 0 ; i < n; i++) {
+			if(fruits[i] <= l ) {
+				l+=1;
 			}
 		}
-		
+	
 		sb.append(l);
 		
 
