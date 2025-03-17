290483ff3a076b9504fce3efa27c95122037f54a
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-03-17 17:09:01 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 229 ms, Memory: 49,992 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -1,42 +1,54 @@
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for _ in range(1, T + 1):
-    test_case, n = input().split()
-    arr = list(input().split())
 
-    gnsToNumber = {
-        "ZRO": 0,
-        "ONE": 1,
-        "TWO": 2,
-        "THR": 3,
-        "FOR": 4,
-        "FIV": 5,
-        "SIX": 6,
-        "SVN": 7,
-        "EGT": 8,
-        "NIN": 9,
-    }
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
-    gnsToStr = {
-        0: "ZRO",
-        1: "ONE",
-        2: "TWO",
-        3: "THR",
-        4: "FOR",
-        5: "FIV",
-        6: "SIX",
-        7: "SVN",
-        8: "EGT",
-        9: "NIN",
-    }
+public class Solution {
 
-    for i in range(int(n)):
-        arr[i] = int(gnsToNumber[arr[i]])
+	static HashMap<String, Integer> numberMap = new HashMap<>();
+	static String[] stringArr = { "ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN" };
 
-    arr.sort()
+	public static void main(String[] args) throws Exception {
 
-    for i in range(int(n)):
-        arr[i] = gnsToStr[arr[i]]
+		numberMap.put("ZRO", 0);
+		numberMap.put("ONE", 1);
+		numberMap.put("TWO", 2);
+		numberMap.put("THR", 3);
+		numberMap.put("FOR", 4);
+		numberMap.put("FIV", 5);
+		numberMap.put("SIX", 6);
+		numberMap.put("SVN", 7);
+		numberMap.put("EGT", 8);
+		numberMap.put("NIN", 9);
 
-    print(test_case)
-    print(' '.join(arr))
+		// System.setIn(new FileInputStream("테스트케이스.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int T = Integer.parseInt(st.nextToken());
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine().trim());
+			sb.append(st.nextToken()).append(" " );
+			int n = Integer.parseInt(st.nextToken());
+
+			int[] numInt = new int[n];
+			st = new StringTokenizer(br.readLine().trim());
+			for (int i = 0; i < n; i++) {
+				numInt[i] = numberMap.get(st.nextToken());
+			}
+
+			Arrays.sort(numInt);
+
+			for (int i = 0; i < n; i++) {
+				sb.append(stringArr[numInt[i]]).append(" ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
