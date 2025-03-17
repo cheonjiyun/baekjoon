300f5c57d042dedac26b240ab53098b199cd3e81
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-03-17 14:10:23 +0900

    [Bronze II] Title: 피보나치 수, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int n = Integer.parseInt(st.nextToken());
+
+		int[] fib = new int[n+2];
+		fib[0] = 1;
+		fib[1] = 1;
+		for (int i = 2; i < n; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+
+		sb.append(fib[n - 1]);
+
+		System.out.println(sb);
+	}
+}
