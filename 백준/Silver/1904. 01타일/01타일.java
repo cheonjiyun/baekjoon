ef5cf4e9c7b8ebde9a492da1a2aa87c323778c2f
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-03-24 23:20:09 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18180 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+
+    // System.setIn(new FileInputStream("테스트케이스.txt"));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    StringTokenizer st = new StringTokenizer(br.readLine().trim());
+    int n = Integer.parseInt(st.nextToken());
+
+    int[] dp = new int[n + 1];
+
+    for (int i = 1; i <= n; i++) {
+      if (i == 1) {
+        dp[i] = 1;
+      } else if (i == 2) {
+        dp[i] = 2;
+      } else {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+      }
+    }
+
+    sb.append(dp[n]);
+
+    System.out.println(sb);
+  }
+}
