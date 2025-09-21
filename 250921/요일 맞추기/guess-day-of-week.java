14f08088cff9974f7ecce3c9edce6ffd7a3b0007
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-09-21 12:01:27 +0900

    Title: 요일 맞추기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250921/요일 맞추기/guess-day-of-week.java
+++ 250921/요일 맞추기/guess-day-of-week.java
@@ -7,23 +7,26 @@ public class Main {
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
         // Please write your code here.
-
-        int[] numOfDays = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-
-        int days = 0;
-        for(int i = m1; i <= m2; i++){
-            days += numOfDays[i];
-        }
-        
-        days -= d1 - 1;
-        days -= numOfDays[m2] - d2 + 1;
+        int days = getTime(m2,d2) - getTime(m1,d1);
         
         while(days < 0){
             days += 7;
         }
 
-        String[] strOfDay = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-        
+      
         System.out.println(strOfDay[days % 7]);
+    }
+
+    static int[] numOfDays = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    static String[] strOfDay = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        
+    public static int getTime(int m, int d){
+        int days = 0;
+        for(int i = 1; i < m; i++){
+            days += numOfDays[i];
+        }
+        days += d;
+
+        return days;
     }
 }
