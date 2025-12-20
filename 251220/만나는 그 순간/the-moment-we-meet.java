c4db683e181dbd18e76228742e24f42f675cbdb9
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-12-20 16:38:08 +0900

    Title: 만나는 그 순간, Time: 151ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 251220/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arrN = new int[1000 * 1000];
+        int[] arrM = new int[1000 * 1000];
+
+        int time = 0;
+        int currentLoc = 0;
+
+        for(int i = 0; i < n; i++){
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+
+
+            for(int j = 0; j < t; j++){
+                time++;
+                if(d == 'L'){
+                    currentLoc--;
+                }else if(d == 'R'){
+                    currentLoc++;
+                }
+                arrN[time] = currentLoc; 
+            }
+        }
+
+        time = 0;
+        currentLoc = 0;
+        for(int i = 0; i < m; i++){
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+
+            for(int j = 0; j < t; j++){
+                time++;
+                if(d == 'L'){
+                    currentLoc--;
+                }else if(d == 'R'){
+                    currentLoc++;
+                }
+                arrM[time] = currentLoc; 
+            }
+        }
+
+        for(int i = 1; i < 1000 * 1000; i++){
+           // System.out.println(arrN[i] + " " + arrM[i]);
+            if(arrN[i] == arrM[i]){
+                System.out.println(i);
+                return;
+            }
+        }
+        
+        System.out.println(-1);
+        // Please write your code here.
+    }
+}
