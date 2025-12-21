3720e5a7096b7a77d04d3d08550e5826bff238cd
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-12-22 01:21:35 +0900

    Title: 좌우로 움직이는 로봇, Time: 130ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251222/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class Main {
+
+    static int MAX = 1_000_001;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] location = new int[2][MAX];
+        
+        int maxTime =0;
+        int timeA = 0;
+        for (int i = 0; i < n; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+            // Please write your code here.
+
+            while(t-- > 0){
+                timeA++;
+                maxTime = Math.max(maxTime, timeA);
+                if(d == 'L'){
+                    location[0][timeA] = location[0][timeA - 1] - 1;
+                }else{
+                    location[0][timeA] = location[0][timeA - 1] + 1;
+                }
+            }
+        }
+        
+        int timeB = 0;
+        for (int i = 0; i < m; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+            
+            while(t-- > 0){
+                timeB++;
+                maxTime = Math.max(maxTime, timeB);
+                
+                if(d == 'L'){
+                    location[1][timeB] = location[1][timeB - 1] - 1;
+                }else{
+                    location[1][timeB] = location[1][timeB - 1] + 1;
+                }
+            }
+        }
+
+        for(int i = timeA + 1; i <= maxTime; i++){
+            System.out.print(location[0][i] + " ");
+        }
+        System.out.println();
+        for(int i = 1; i <= maxTime; i++){
+            System.out.print(location[1][i] + " ");
+        }
+        System.out.println();
+        
+        boolean together = true;
+        int count = 0;
+        for(int i = 1; i <= maxTime; i++){
+            
+            if(!together && location[0][i] == location[1][i]){
+                System.out.println(i);
+                count++;
+            }
+            if(location[0][i] == location[1][i]){
+                together = true;
+            }else{
+                together = false;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
