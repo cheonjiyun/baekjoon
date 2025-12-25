f117a533e36b57e823e7c05e99fdf75bf10053ff
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-12-25 22:27:30 +0900

    Title: 되돌아오기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251225/되돌아오기/come-back.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int time = 0;
+
+        int curR = 0;
+        int curC = 0;
+
+        for(int i = 0; i < n; i++){
+            char dir = sc.next().charAt(0);
+            int d = getDir(dir);
+            int dist = sc.nextInt();
+
+            for(int t = 0; t < dist; t++){
+                time++;
+
+                curR += dr[d];
+                curC += dc[d];
+
+                if(curR == 0 && curC == 0){
+                    System.out.println(time);
+                    return;
+                }
+            }
+        }
+        // Please write your code here.
+    }
+
+    static int getDir(char d){
+        if(d == 'N'){
+            return 0;
+        }else if(d == 'E'){
+            return 1;
+        }else if(d == 'S'){
+            return 2;
+        }else{
+            return 3;
+        }
+    }
+}
