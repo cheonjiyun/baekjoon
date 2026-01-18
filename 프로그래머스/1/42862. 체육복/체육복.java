b85214352357f0c57a3d1da475a4251463347a61
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2026-01-18 14:34:19 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        
+        int[] clothes = new int[n+2];
+        for(int i = 1; i < n+1; i++){
+            clothes[i] += 1;
+        }
+        
+        for(int l : lost){
+            clothes[l] -= 1;
+        }
+        for(int r : reserve){
+            clothes[r] += 1;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            if(clothes[i] == 0){
+                if(clothes[i-1] >= 2){
+                    clothes[i-1] -= 1;
+                    clothes[i] += 1;
+                }else if(clothes[i+1] >= 2){
+                    clothes[i+1] -= 1;
+                    clothes[i] += 1;
+                }
+            }
+        }
+        
+        int count = 0;
+        for(int i = 1; i <= n; i++){
+            if(clothes[i] >= 1){
+                count++;
+            }
+        }
+        
+        
+        return count;
+    }
+}
